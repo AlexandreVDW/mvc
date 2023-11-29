6e92fd2ea761be5bd1915299d77590d0181b6b62
alexandreVDW <vandewielealexandre@gmail.com>
2023-11-29 09:42:07 +0100

    first commit of day

--- View/home.php
+++ View/home.php
@@ -3,7 +3,7 @@
 <?php // Use any data loaded in the controller here ?>
 
 <section>
-    <h1>This is a mvc projetc :</h1>
+    <h1>This is a mvc project :</h1>
     <p>I think it can give you AVC and </p>
     <p>Big mental issues</p>
 </section>
